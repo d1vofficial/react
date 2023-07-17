0cffb2f018b26ca64081908ad11682de32e0ac13
d1vofficial <d1vofficial@users.noreply.github.com>
2023-07-17 16:39:29 +0530

    warning fixtures

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import HeroImage from "../assets/heroImage.png";
-import { MdOutlineKeyboardArrowRight } from "react-icons/md";
+// import { MdOutlineKeyboardArrowRight } from "react-icons/md";
 
 const Home = () => {
   return (
